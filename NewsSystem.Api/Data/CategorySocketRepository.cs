95906af2c2e6c64fc480fbd6d3515a8f7a240883
Dragomir Balinov <dnbalinov@gmail.com>
2014-12-23 16:56:50 +0200

    Add category socket repository class.

---
+++ NewsSystem.Api/Data/CategorySocketRepository.cs
@@ -0,0 +1,43 @@
+﻿namespace NewsSystem.Api.Data
+{
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Xml.Serialization;
+    using NewsSystem.Api.Models;
+    using NewsSystem.Api.Socket;
+
+    public class CategorySocketRepository : IRepository<Category>
+    {
+        public Category Get(int id)
+        {
+            var request = new SocketRequest()
+            {
+                EntityType = "Category",
+                Method = "Get",
+                Params = new Dictionary<string, object>()
+            };
+            request.Params["id"] = id;
+
+            // Send Request.
+            var serializer = new XmlSerializer(typeof(SocketRequest));
+            using (var writer = new StringWriter())
+            {
+                serializer.Serialize(writer, request);
+                string a = writer.ToString();
+            }
+
+            return null;
+        }
+
+        public IEnumerable<Category> All()
+        {
+            var request = new SocketRequest()
+            {
+                EntityType = "Category",
+                Method = "All"
+            };
+
+            return null;
+        }
+    }
+}
---
+++ NewsSystem.Api/Socket/SocketRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace NewsSystem.Api.Socket
+{
+    using System.Collections.Generic;
+
+    public class SocketRequest
+    {
+        public string EntityType { get; set; }
+
+        public string Method { get; set; }
+
+        public IDictionary<string, object> Params { get; set; }
+    }
+}
