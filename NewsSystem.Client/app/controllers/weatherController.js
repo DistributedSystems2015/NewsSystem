d356ea10d7e944535f48090ba35a0e6b89132ae9
Dragomir Balinov <dnbalinov@gmail.com>
2015-01-03 10:24:17 +0200

    - Round display number for degrees celsius

--- NewsSystem.Client/app/controllers/weatherController.js
+++ NewsSystem.Client/app/controllers/weatherController.js
@@ -2,6 +2,6 @@
     var promise = weatherService.getWeather();
     promise.then(function (item) {
         $scope.icon = item.icon;
-        $scope.deg = item.deg;
+        $scope.deg = Math.round(item.deg * 100) / 100;
     });
 }
